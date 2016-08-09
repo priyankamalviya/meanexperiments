379898fbbf53e0a399ca6c7f2ab2a544385efba1
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-08-09 15:46:15 -0700

    quiz controller logic

--- angularQuiz/js/controllers/quiz.js
+++ angularQuiz/js/controllers/quiz.js
@@ -10,6 +10,37 @@
 
             vm.quizMetrics = quizMetrics;
             vm.dataService = DataService;
+            vm.questionAnswered = questionAnswered;
+            vm.setActiveQuestion = setActiveQuestion;
+            vm.activeQuestion = 0;
+
+
+            var numQuestionsAnswered =0;
+
+            function setActiveQuestion(){
+                var breakOut = false;
+                var quizLength = DataService.quizQuestions.length - 1;
+
+                while(!breakOut){
+                    vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+
+                    if(dataService.quizQuestions[vm.activeQuestion].selected ===null){
+                        breakOut = true;
+                    }
+                }
+            }
+            
+            function questionAnswered() {
+
+                var quizLength = DataService.quizQuestions.length;
+                if(DataService.quizQuestions[vm.activeQuestion].selected !== null){
+                    numQuestionsAnswered++;
+                    if(numQuestionsAnswered >=quizLength){
+                        //finalise the quiz
+                    }
+                }
+                vm. setActiveQuestion();
+            }
         }
 
 
