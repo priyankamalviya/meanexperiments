d6106e15d861567555656a0da1b30c833638b31a
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-08-11 20:54:47 -0700

    adding mark up to correct and incorrect answers

--- angularQuiz/js/controllers/results.js
+++ angularQuiz/js/controllers/results.js
@@ -8,6 +8,21 @@
         function ResultsController($scope, quizMetrics, DataService){
             var vm = $scope;
             vm.quizMetrics = quizMetrics;
-            vm.DataService = DataService;
+            vm.dataService = DataService;
+            vm.getAnswerClass =getAnswerClass;
+            vm.activeQuestion = 0;
+
+
+            function getAnswerClass(index){
+
+                if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
+                    return "bg-success";
+                }else if(index === dataService.quizQuestions[vm.activeQuestion].selected){
+                    return "bg-danger";
+                }
+
+            }
         }
+
+
 })();
